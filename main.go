82c0558b60de0ca847fa3f052ac48b3b36186bf0
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-04-13 15:59:35 +0300

    some fixes

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"pamukkale_university/database"
 	"pamukkale_university/env"
 	"pamukkale_university/routes"
@@ -11,7 +10,4 @@ func main() {
 	env.InitProd()
 	database.Init()
 	routes.InitProd()
-	log.Println(env.Getenv(env.DB_CONNECTION))
-	log.Println(env.Getenv(env.DB_EXTERNAL_URL))
-	log.Println(env.Getenv(env.PORT))
 }
